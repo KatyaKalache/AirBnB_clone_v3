1e5a92c1cc14e381ba2b2be07a02268ddd580c64
Spencer <spencerhcheng@gmail.com>
2017-09-13 22:59:06 +0000

    index: Add endpoint for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import Flask, render_template, jsonify
+from models import storage
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+class_dict = {
+    'Amenity': 'amenities',
+    'City': 'cities',
+    'Place': 'places',
+    'Review': 'reviews',
+    'State': 'states',
+    'User': 'users'
+    }
+
 
 @app_views.route('/status')
 def json_status():
     """Returns JSON status"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats')
+def example():
+    """Retrieves number of each objects by type"""
+    count_dict = {}
+    for key, value in class_dict.items():
+        count_dict[value] = storage.count(key)
+    return jsonify(count_dict)
