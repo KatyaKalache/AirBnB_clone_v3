6eb557311ddf2eb37872fe3aa86a59422643d892
KatyaKalache <katya@kalache.fr>
2017-09-15 05:04:03 +0000

    adding amenity, rfeviews, init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,8 @@ from flask import Blueprint, render_template
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 
+from api.v1.views.amenities import *
 from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.index import *
+from api.v1.views.reviews import *
 from api.v1.views import app_views
---
+++ api/v1/views/reviews.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+"""Create a new view for State objects"""
+from flask import request, abort
+from models import storage, Review
+from api.v1.views import app_views
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+@app_views.route('/reviews', strict_slashes=False)
+def get_reviews():
+    """Retrieves the list of all Review objects of a Place"""
+    reviews_list = []
+    for key, value in storage.all("Review").items():
+        reviews_list.append(value.to_json())
+    return (jsonify(reviews_list))
+
+
+@app_views.route('places/<place_id>/reviews', strict_slashes=False)
+def reviews_by_place(place_id):
+    """Retrieves the list of all Review objects of a Place"""
+    all_list = []
+    review_list = []
+    place = storage.get("Place", place_id)
+
+    if place is None:
+        abort(404)
+
+    for key, value in storage.all("Place").items():
+        all_list.append(v.to_json())
+
+    for review in all_list:
+        if review['place_id'] == place.id:
+            review_list.append(review)
+
+    return (jsonify(review_list))
+
+
+@app_views.route('/amenities/<review_id>', strict_slashes=False)
+def get_review_object(review_id):
+    """Retrieves a Review object"""
+    review = storage.get("Review", review_id)
+
+    if review is None:
+        abort(404)
+
+    return (jsonify(review.to_json()))
+
+
+@app_views.route('/reviews/<review_id>', strict_slashes=False)
+def delete_review(review_id):
+    """Deletes a review by its id"""
+    empty_dict = {}
+
+    review = storage.get("Review", review_id)
+
+    if review is None:
+        abort(404)
+    else:
+        storage.delete(review)
+        storage.save()
+        return jsonify(empty_dict), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def create_review(review_id):
+    """Creates a new review"""
+    req = request.get_json()
+
+    review = storage.get("Review", review_id)
+
+    if review is None:
+        abort(404)
+
+    if req is None:
+        return (jsonify("Not a JSON"), 400)
+
+    try:
+        req['user_id']
+    except:
+        return (jsonify("Missing user_id"), 400)
+
+    try:
+        req['text']
+    except:
+        return (jsonify("Missing text")
+
+    req['user_id'] = user_id
+    review_data = Review(**req)
+
+    for key, value in req.items():
+        setattr(review_data, key, value)
+
+    review_data.save()
+    return (jsonify(review_data.to_json()), 201)
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_review(review_id):
+    """Updates Review object"""
+    review = storage.get("Review", review_id)
+    req = request.get_json()
+
+    if review is None:
+        abort(404)
+    if req is None:
+        return(jsonify("Not a JSON"), 400)
+
+    for key, value in req.items():
+        setattr(review, key, value)
+
+    review.save()
+
+    return (jsonify(review.to_json()), 200)
